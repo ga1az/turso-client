0bf4485286b59c6afeec472684ca00ab803749ce
ga1az <ga1az@outlook.com>
2024-07-20 17:26:27 -0400

    chore: Update test file to use bun:test for testing locations

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,18 @@
 import { TursoClient } from "../src";
 import { config } from "dotenv";
+import { expect, test } from "bun:test";
 config({
   path: "../.env",
 });
 const TOKEN = process.env.TURSO_API_KEY;
 
-const client = new TursoClient({ apiKey: TOKEN! });
+if (!TOKEN) {
+  throw new Error("TURSO_API_KEY is not set in the environment variables");
+}
 
-client.locations.list().then((res) => {
-  console.log(res);
+test("Get locations", async () => {
+  const client = new TursoClient({ apiKey: TOKEN });
+  const locations = await client.locations.list();
+
+  expect(locations).toHaveProperty("locations");
 });
